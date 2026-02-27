622428d4b415c4aab4532343f24592f52b39f26a
Mohd.Anas <anas@billioninfotech.com>
2026-02-27 17:22:47 +0530

    Remove Next.js configuration options for output, images, and React Compiler.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-
-  reactCompiler: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
